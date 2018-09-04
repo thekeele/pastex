041ef236a8d12322e8cb6a10f3e9cb5b071df02c
Mark Keele <mark@onxmaps.com>
2018-09-04 10:40:56 -0700

    add an error

--- lib/pastex_web/schema.ex
+++ lib/pastex_web/schema.ex
@@ -5,5 +5,9 @@ defmodule PastexWeb.Schema do
     field :health, :string do
       resolve fn _, _, _ -> {:ok, "up"} end
     end
+
+    field :explode, :string do
+      resolve fn _, _, _ -> {:error, "kaboom!"} end
+    end
   end
 end
